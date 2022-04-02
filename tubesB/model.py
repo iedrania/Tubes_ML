7e21e1ce850bfad70ab7337c18175b411fbfefd5
zaidannaufal <13518021@std.stei.itb.ac.id>
2022-04-02 09:14:04 +0700

    Co-authored-by: Siti Iedrania <iedrania@users.noreply.github.com> Co-authored-by: Delisha Azza Naadira <delishaandr@users.noreply.github.com> Co-authored-by: Karina Imani <imanikarina@users.noreply.github.com>

--- tubesB/model.py
+++ tubesB/model.py
@@ -31,7 +31,7 @@ class Model:
                 n_perceptron = int(f.readline())
                 arr_act_func.append(f.readline().rstrip("\n"))
                 for _ in range(n_perceptron):
-                    arr = [int(x) for x in f.readline().split(" ")]
+                    arr = [float(x) for x in f.readline().split(" ")]
                     new_perceptron = Perceptron(np.array(arr))
                     layer_arr.append(new_perceptron)
                 arr_act_perc.append(layer_arr)
@@ -44,38 +44,100 @@ class Model:
     def _buildModel(self):
         arr_layer = []
         for i in range(self.total_layer-1):
-            if (i == total_layer-2): # layer output?
+            if (i == self.total_layer-2): # layer output
                     arr_layer.append(Layer("output", self.arr_act_func[i], self.arr_act_perc[i]))
-                else: # layer hidden?
-                    arr_layer.append(Layer("hidden", self.arr_act_func[i], self.arr_act_perc[i]))
+            else: # layer hidden
+                arr_layer.append(Layer("hidden", self.arr_act_func[i], self.arr_act_perc[i]))
         return arr_layer
     
     def doffnn(self,inputarr):
         arr = inputarr
         for layer in self.model:
-            bias = [1]*len(inputarr[0])
-            # bias = [1,1,1,1]
+            arr = np.transpose(arr)
+            bias = [1]*len(arr[0])
             arr = np.append([bias],arr,0)
             arr = layer.calculate_all(np.transpose(arr))
         return arr
+    
+    def dobackwardpropagation(self, learning_rate, target):
+        for i in reversed(range(len(self.model))) :
+            layer = self.model[i]
+            if (layer.type == "output"):
+                temp_error, weight_before = layer.calculate_back_output(learning_rate, target)
+            else:
+                temp_error = layer.calculate_back_hidden(learning_rate, temp_error, weight_before)
+
+    def fit(self,inputarr, target, learningrate,errorthreshold, maxiter):
+        loss = 9999999
+        for _ in range(maxiter):
+            for i in range (len(inputarr)):
+                output = self.doffnn(inputarr[i])
+            # loss = self.crossentropy(output) if (self.model[-1].activation_function_type=="softmax") else self.sumsquarederror(output,target)
+            # if loss <= errorthreshold:
+            #     return output, loss          
+            self.dobackwardpropagation(learningrate, target)
+        return output
+            
+
+    def sumsquarederror(self,output,target): # target = iris_y
+        sigma = 0
+        for i in range(len(output)):
+            sigma += target[i] - output[i]
+        return 0.5*sigma^2
+
+    # def crossentropy(self,output):
+    #     return -np.log(output)
+
+    def printw(self):
+         for layer in self.model:
+             for p in layer.array_of_perceptron:
+                 print(p.weights)
 
 if __name__ == "__main__":
+    
     iris_X, iris_y = load_iris(return_X_y = True)
-    print(iris_X[10], iris_y[10])
-    print(np.append(iris_X[10], iris_y[10]))
-    # bagi iris_X (dan iris_y) sesuai batch_size
-    batch_size = 5 # self?
-    n_batch = int(np.ceil(len(iris_X)/batch_size))
-    iris_X_batches = [[0 for j in range (batch_size)] for i in range (n_batch)]
-    for i in range (n_batch):
-        for j in range (batch_size):
-            if (5*i+j < len(iris_X)):
-                iris_X_batches[i][j] = iris_X[5*i+j]
-            else:
-                iris_X_batches[i][j] = 0 # handle batch kekurangan anggota
-    for i in range (len(iris_X_batches)):
-        print(iris_X_batches[i])
+    # print(iris_X[10], iris_y[10])
+    # print(np.append(iris_X, iris_y))
+    # # bagi iris_X (dan iris_y) sesuai batch_size
+    # batch_size = 5 # self?
+    # n_batch = int(np.ceil(len(iris_X)/batch_size))
+    # iris_X_batches = [[0 for j in range (batch_size)] for i in range (n_batch)]
+    # for i in range (n_batch):
+    #     for j in range (batch_size):
+    #         if (batch_size*i+j < len(iris_X)):
+    #             np.append(iris_X_batches[i], iris_X[5*i+j])
+    #         else:
+    #             iris_X_batches[i][j] = 0 # todo handle batch kekurangan anggota
+    
+    # bikin mini batch
+    model = Model("model.txt")
+
+    mini_batch_X = []
+    mini_batch_y = []
+    batches_X = []
+    batches_y = []
+    for i in range (len(iris_X)):
+        mini_batch_X.append(iris_X[i])
+        mini_batch_y.append(iris_y[i])
+        if (i % model.batch_size == model.batch_size - 1): # isi mini_batch sebanyak batch_size
+            batches_X.append(mini_batch_X)
+            batches_y.append(mini_batch_y)
+            mini_batch_X = []
+            mini_batch_y = []
+    print(model.fit(batches_X, batches_y, 0.1, 1, 10))
+
+    # for i in range (len(batches_X)):
+    #     print(i)
+    #     print(batches_X[i])
+    #     print(batches_y[i])
     # apakah yg namanya batch itu gini ._.
     # for i in range(epoch):
     #     no_of_batches = len(X_train) // N
     #     for j in range(no_of_batches):
+    
+    # arr1 = np.array([0,0,1,1])
+    # arr2 = np.array([0,1,0,1])
+    # array_input = np.array([arr1, arr2])
+    # target = np.array([1,0,0,1])
+    # model = Model("model.txt")
+    # model.fit(array_input, target, 0.05,0.01,10)
--- tubesB/perceptron.py
+++ tubesB/perceptron.py
@@ -1,3 +1,6 @@
+from distutils.log import error
+import errno
+from black import err
 import numpy as np
 # from torch import sigmoid
 
@@ -5,63 +8,113 @@ class Perceptron:
 
     def __init__(self, arr):
         self.weights = arr
+        self.outputs = np.array([])
+        self.inputs = np.array([])
+        self.errors = np.array([])
 
     def calculate(self, arr, actifunct):
+        self.inputs = arr
         res = np.matmul(arr, self.weights)
         if (actifunct == "linear"):
-            return res
+            self.outputs = res
         elif (actifunct == "sigmoid"):
-            return self.sigmoid(res)
+            self.outputs = self.sigmoid(res)
         elif (actifunct == "relu"):
-            return self.relu(res)
+            self.outputs = self.relu(res)
         elif (actifunct == "softmax"):
-            return self.softmax(res)
+            self.outputs = self.softmax(res)
+        return self.outputs
 
     def sigmoid(self, arr):
         return 1/(1+np.exp(-arr))
     
     def relu(self, arr):
-        return np.maximum(arr,0)
+        return np.maximum(arr)
 
     def softmax(self, arr):
         return np.exp(arr)/np.sum(np.exp(arr))
 
-    def updateweight(self,lr):
-        pass
-        new = np.zeros_like(self.weights)
+    def error_softmax(self, num):
+        return -np.log(num)
+    
+    def d_linear(self):
+        self.outputs.fill(1)
+        return self.outputs
+
+    def d_relu(self):
+        self.outputs[self.outputs >= 0] = 1
+        self.outputs[self.outputs < 0] = 0
+        return self.outputs
+    
+    def d_sigmoid(self):
+        sigm = self.sigmoid(self.outputs)
+        return sigm * (1 - sigm)
+
+    def calculate_gradient_softmax(self, target):
+        return (self.outputs - target) * self.inputs
+
+    def update_weight_output_softmax(self, learning_rate, target):
+        gradient = self.calculate_gradient_softmax(target)
+        for i in range(len(error)):
+            self.errors = np.append(self.errors, self.error_softmax(self.outputs[i])) 
         for i in range(len(self.weights)):
-            new[i] = self.arr
-        
-    def sumsquarederror(self):
-        pass
+            self.weights[i] = self.weights[i] - learning_rate * gradient[i]
+ 
+    def update_weight_hidden(self, learning_rate, actifunct, error, weight): # error dari layer di depannya
+        # calculate errors and gradients
+        for i in range(len(error)):
+            self.errors = np.append(self.errors, np.multiply(error[i], weight))
+        gradient = self.calculate_gradient(actifunct)
+        sum_gradient = np.sum(gradient, axis = 0)
+        # update weight
+        for i in range(len(self.weights)):
+            self.weights[i] = self.weights[i] - learning_rate * sum_gradient[i]
 
-    def crossentropy(self):
-        pass
-    
-    def d_linear(self,arr):
-        pass
+    def update_weight_output(self, learning_rate, actifunct, target):
+        # calculate errors and gradients
+        self.errors = np.subtract(target, self.outputs)
+        gradient = self.calculate_gradient(actifunct)
+        sum_gradient = np.sum(gradient, axis = 0)
+        # update weight
+        for i in range(len(self.weights)):
+            self.weights[i] = self.weights[i] - learning_rate * sum_gradient[i]
 
-    def d_relu(self,arr):
-        pass
-        arr[arr < 0] = 0
-        arr[arr >= 0] =1
-        return arr
-    
-    def d_sigmoid(self,arr):
-        pass
-        temp = self.sigmoid()
-        return sigmoid
-
-    def d_softmax(self,arr):
-        pass
-        pj if j != targetClass
-        -(1-pj) if j == targetClass
-        
+    def calculate_gradient(self, actifunct):
+        if (actifunct == "linear"):
+            return -self.errors * self.d_linear() * np.transpose(self.inputs)
+        elif (actifunct == "relu"):
+            return -self.errors * self.d_relu() *  np.transpose(self.inputs)
+        elif (actifunct == "sigmoid"):
+            return -self.errors * self.d_sigmoid() * np.transpose(self.inputs)
+        # elif (actifunct == "softmax"):
+        #     return self.d_softmax() * self.inputs
 
 if __name__ == "__main__":
-    perc = Perceptron(np.array([1,2,3]))
-    arr1 = np.array([1,0,0])
-    arr2 = np.array([1,0,1])
-    arr3 = np.array([1,1,0])
-    arr4 = np.array([1,1,1])
-    print(perc.calculate(np.array([arr1,arr2,arr3,arr4]), "sigmoid"))
+    # perc = Perceptron(np.array([1,2,3]))
+    # arr1 = np.array([1,0,0])
+    # arr2 = np.array([1,0,1])
+    # arr3 = np.array([1,1,0])
+    # arr4 = np.array([1,1,1])
+    # print(perc.calculate_(np.array([arr1,arr2,arr3,arr4]), "sigmoid"))
+    a = [0.5,0.5,0.5,0.5]
+    temp = np.empty((0,len(a)), float)
+    temp = np.append(temp,([a]),axis=0)
+    # temp2 = np.array([0.5],)
+    # print(np.append(temp,temp2))
+    print(temp)
+    # a = np.array([])
+    # b = np.append(np.array([1,2,3,4]))
+    # print(np.append(a,b))
+
+def calculate_back_output(learning_rate, target):
+    arr_error = np.array([])
+    arr_weights = np.array([])
+    for i in range (len(self.array_of_perceptron)):
+        perceptron = self.array_of_perceptron[i]   
+        perceptron.update_weight_output(learning_rate, self.activation_function_type, target)
+        arr_error = np.append(arr_error, perceptron.errors)
+        arr_weights = np.append(arr_weights, perceptron.weights)
+        print(arr_error)
+    arr_error = np.array(arr_error)
+    print(arr_error)
+    return arr_error, arr_weights
